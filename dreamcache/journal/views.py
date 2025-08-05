c939146415d04b62404a75cb994e4cfd2a732072
Christian <kalonjic34@gmail.com>
2025-08-05 16:34:28 +0200

    Implement basic views (homepage, register, my_login, dashboard)

--- dreamcache/journal/views.py
+++ dreamcache/journal/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def homepage(request):
+    pass
+
+def register(request):
+    pass
+
+def my_login(request):
+    pass
+
+def dashboard(request):
+    pass
