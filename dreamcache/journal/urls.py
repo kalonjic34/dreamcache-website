3a4cb84aad3dc887528bbd6e9336bd7638015cd0
Christian <kalonjic34@gmail.com>
2025-08-06 15:35:30 +0200

    Rename 'my_login' to 'my-login' in urls

--- dreamcache/journal/urls.py
+++ dreamcache/journal/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.homepage, name=""),
     path('register',views.register, name="register"),
-    path('my_login',views.my_login, name="my_login"),
+    path('my-login',views.my_login, name="my-login"),
     path('dashboard',views.dashboard, name="dashboard"),
 
 ]
