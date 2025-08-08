90bfa675f2865003efbba0650eb4c4ab1f8b10d7
Christian <kalonjic34@gmail.com>
2025-08-08 14:03:34 +0200

    Implement Thought model for user journal

--- dreamcache/journal/models.py
+++ dreamcache/journal/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Thought(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.CharField(max_length=400)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    
+    user =  models.ForeignKey(User, max_length=10, on_delete=models.CASCADE, null=True)
+    
+    
