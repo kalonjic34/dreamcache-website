d37c5dfd62a3200f847ca490c0e3ff97bf68c64d
Christian <kalonjic34@gmail.com>
2025-08-12 11:51:05 +0200

    Implement password reset feature

--- dreamcache/journal/urls.py
+++ dreamcache/journal/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.homepage, name=""),
@@ -13,5 +14,10 @@ urlpatterns = [
     path('delete-thought/<str:pk>',views.delete_thought, name="delete-thought"),
     path('profile-management',views.profile_management, name="profile-management"),
     path('delete-account',views.delete_account, name="delete-account"),
+    
+    path('reset_password', auth_views.PasswordResetView.as_view(template_name='journal/password-reset.html'), name='reset_password'),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name='journal/password-reset-sent.html'), name='password_reset_done'),
+    path('reset/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='journal/password-reset-form.html'), name='password_reset_confim'),
+    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(template_name='journal/password-reset-complete.html'), name='password_reset_complete')
     
 ]
